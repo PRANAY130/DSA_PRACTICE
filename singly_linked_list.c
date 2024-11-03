b22110ca6f3ecdab48c8167a4eee192b746c7db2
Alhaitham <pranayde201@gmail.com>
2024-11-04 00:44:51 +0530

    done

--- singly_linked_list.c
+++ singly_linked_list.c
@@ -30,6 +30,34 @@ struct Node * createLinkedList (int n){
     return head;
 }
 
+void insertatanyposition(struct Node** head, int data, int index){
+    struct Node* newnode=(struct Node*)malloc(sizeof(struct Node));
+    struct Node* temp=*head;
+    newnode->data=data;
+    newnode->next=NULL;
+    if(*head==NULL){
+        printf("The list is empty. Inserting at the beginning...\n");
+        *head=newnode;
+    }
+
+    if(index==1){
+        newnode->next=*head;
+        *head=newnode;
+        return;
+    }
+    for (int i=1;temp!=NULL && i<index-1;i++){
+        temp=temp->next;
+    }
+    if(temp==NULL){
+        printf("The position %d is not present.insertion is not possible\n");
+        free(newnode);
+        return;
+    }
+    newnode->next=temp->next;
+    temp->next=newnode;
+}
+
+
 int main(){
     struct Node * head = NULL;\
     int n;
