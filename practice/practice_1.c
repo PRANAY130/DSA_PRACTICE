21906c94c26477282db5d9a99f7d8f8c95f9f67e
Alhaitham <pranayde201@gmail.com>
2025-01-16 19:36:49 +0530

    stack implementation

--- practice/practice_1.c
+++ practice/practice_1.c
@@ -6,6 +6,28 @@ int top=-1;
 void push(char ch){
     if (top==MAX-1){
         printf("Stack overflow\n");
+        return;
     }
     stack[++top]=ch;
+}
+char pop(){
+    if(top==-1){
+        printf("The stack is empty.\n");
+        return;
+    }
+    char popped_element=stack[top--];
+    return popped_element;
+}
+
+char peek(){
+    char peeked_element=stack[top];
+    return peeked_element;
+}
+
+int isempty(){
+    return (top==-1);
+}
+
+int isfull(){
+    return (top==MAX-1);
 }
