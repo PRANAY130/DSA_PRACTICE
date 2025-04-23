2a7bade76c2541376f791fbc5b2b71f08bbe1c61
Alhaitham <pranayde201@gmail.com>
2025-04-23 19:35:36 +0530

    y

--- DSA/8_Queens.c
+++ DSA/8_Queens.c
@@ -2,12 +2,15 @@
 
 #define MAX 20
 
-int board[MAX];  // board[i] = column position of queen at row i
+int board[MAX]; // board[i] = column position of queen at row i
 int N;
 
-void printSolution() {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+void printSolution()
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
             if (board[i] == j)
                 printf("Q ");
             else
@@ -18,35 +21,43 @@ void printSolution() {
     printf("\n");
 }
 
-void solve(int row) {
-    if (row == N) {
+void solve(int row)
+{
+    if (row == N)
+    {
         printSolution();
         return;
     }
 
-    for (int col = 0; col < N; col++) {
+    for (int col = 0; col < N; col++)
+    {
         int safe = 1;
-        for (int i = 0; i < row; i++) {
-            if (board[i] == col ||                         // same column
-                board[i] - i == col - row ||               // same major diagonal
-                board[i] + i == col + row) {               // same minor diagonal
+        for (int i = 0; i < row; i++)
+        {
+            if (board[i] == col ||           // same column
+                board[i] - i == col - row || // same major diagonal
+                board[i] + i == col + row)
+            { // same minor diagonal
                 safe = 0;
                 break;
             }
         }
 
-        if (safe) {
+        if (safe)
+        {
             board[row] = col;
             solve(row + 1);
         }
     }
 }
 
-int main() {
+int main()
+{
     printf("Enter the value of N (1-%d): ", MAX);
     scanf("%d", &N);
 
-    if (N < 1 || N > MAX) {
+    if (N < 1 || N > MAX)
+    {
         printf("Invalid input. Please enter a value between 1 and %d.\n", MAX);
         return 1;
     }
